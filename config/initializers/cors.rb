c7b3b66647fab8f1af6c631b3efc0537f421151f
reito <zhenlinshannei@gmail.com>
2024-09-16 05:20:30 +0900

    origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3001' # フロントエンドのURLを指定
+      # origins 'localhost:3001' # フロントエンドのURLを指定
+      origins 'https://recruitment-app2-frontend.vercel.app/'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
